960cd45a8e56d19bacade786dd6995f7546907e4
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-02-22 20:08:18 +0100

    create user model

---
+++ backend/src/models/user.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  friendList: Array<string>;
+}
+
+const UserSchema: Schema = new Schema({
+  username: { type: String, required: true, minlength: 1, maxlength: 20 },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 254,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 64,
+  },
+  friendList: {
+    type: Array<string>,
+    required: true,
+  },
+});
+
+export const User = mongoose.model<IUser>("User", UserSchema);
