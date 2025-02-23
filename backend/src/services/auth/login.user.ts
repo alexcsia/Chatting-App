1ca4bbe3a86f85b26dfe35d8f2cadfcb26f20644
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-02-23 23:27:32 +0100

    create login service

--- backend/src/services/auth/login.user.ts
+++ backend/src/services/auth/login.user.ts
@@ -1,8 +1,9 @@
 import { ApiError } from "../../api/errors/ApiError";
 import { getUserByEmail } from "../../repositories/userRepo";
+import { comparePassword } from "../../helpers/passwordUtils";
 
 export const loginUser = async (email: string, password: string) => {
-  const user = getUserByEmail(email);
+  const user = await getUserByEmail(email);
   if (!user) throw new ApiError(401, "Email or password is incorrect");
 
   const isMatch = comparePassword(user.password, password);
