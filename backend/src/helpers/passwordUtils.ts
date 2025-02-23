3aad0ad80514fce3c6b7f2f984517323d5e8ba80
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-02-23 23:30:20 +0100

    create comparePasswords function

--- backend/src/helpers/passwordUtils.ts
+++ backend/src/helpers/passwordUtils.ts
@@ -1,4 +1,11 @@
+import bcrypt from "bcrypt";
+
 export const comparePassword = async (
   input: string,
   correctPassword: string
-) => {};
+) => {
+  const hashedInput = await bcrypt.hash(input, 10);
+  const isMatch = await bcrypt.compare(hashedInput, correctPassword);
+
+  return isMatch;
+};
