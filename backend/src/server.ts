7b65c3b6db0ca26725f8384db9750388d469b6dc
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-02-28 23:55:10 +0100

    add profile route as plugin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,19 +3,22 @@ import { connectMongoDB } from "./database";
 import { authRoutes } from "./api/routes/index";
 import cookie from "@fastify/cookie";
 import dotenv from "dotenv";
+import { userRoutes } from "./api/routes/userRoutes/profile";
+
 dotenv.config();
 const PORT = parseInt(process.env.PORT || "3000");
 
 const fastify: FastifyInstance = Fastify({ logger: false });
 
-fastify.register(require(`@fastify/cors`), {
-  origin: `http://localhost:8080`,
-  methods: [`GET`, `POST`],
-  credentials: true,
-});
+// fastify.register(require(`@fastify/cors`), {
+//   origin: `http://localhost:8080`,
+//   methods: [`GET`, `POST`],
+//   credentials: true,
+// });
 
-fastify.register(authRoutes, { prefix: "/auth" });
 fastify.register(cookie);
+fastify.register(authRoutes, { prefix: "/auth" });
+fastify.register(userRoutes);
 
 const start = async () => {
   try {
