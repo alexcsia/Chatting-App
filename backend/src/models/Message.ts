334de8d851a5037c98d6c7f60117505a16086ac7
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-03-11 15:34:57 +0100

    add message model

---
+++ backend/src/models/Message.ts
@@ -0,0 +1,27 @@
+import mongoose, { Date, Document, Schema } from "mongoose";
+
+export interface IMessage extends Document {
+  content: string;
+  username: string;
+  timeStamp: Date;
+  chatId: string;
+}
+
+const MessageSchema: Schema = new Schema({
+  username: { type: String, required: true, minlength: 1, maxlength: 20 },
+  content: {
+    type: String,
+    required: true,
+    maxlength: 1000,
+  },
+  timeStamp: {
+    type: Date,
+    required: true,
+  },
+  chatId: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model<IMessage>("Message", MessageSchema);
