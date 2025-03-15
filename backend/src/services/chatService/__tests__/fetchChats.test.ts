11b5cf5e4576375dd7fb858eab6983c57ebbf5e4
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-03-15 20:43:23 +0100

    add tests for fetchChats

---
+++ backend/src/services/chatService/__tests__/fetchChats.test.ts
@@ -0,0 +1,44 @@
+import { fetchChatIds } from "../fetchChats";
+import mongoose from "mongoose";
+import { User } from "@models/User";
+import { Chat } from "@models/Chat";
+import { ApiError } from "@api/errors/ApiError";
+
+describe("fetchChatIds", () => {
+  beforeAll(async () => {
+    await mongoose.connect("mongodb://localhost:27017/chatdb");
+
+    const user = await User.create({
+      username: "testUsername",
+      password: "123ABcdef!@#",
+      email: "test@test.com",
+    });
+
+    await Chat.create({
+      userIds: user._id,
+    });
+  });
+
+  afterAll(async () => {
+    await User.deleteMany();
+    await Chat.deleteMany();
+    await mongoose.connection.close();
+  });
+
+  it("should fetch a user's chat IDs from the database", async () => {
+    const chatIds = await fetchChatIds("testUsername");
+
+    expect(chatIds).toBeInstanceOf(Array);
+    expect(chatIds.length).toBeGreaterThan(0);
+    expect(chatIds[0]).toBeDefined();
+  });
+
+  it("should throw an error if the user does not exist", async () => {
+    await expect(fetchChatIds("nonExistingUser")).rejects.toThrowError(
+      ApiError
+    );
+    await expect(fetchChatIds("nonExistingUser")).rejects.toThrow(
+      "User not found"
+    );
+  });
+});
