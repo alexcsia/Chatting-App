260331368b6e33ec205dc34c04355583a2653f1a
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-03-15 20:42:52 +0100

    add tests for createMessage

---
+++ backend/src/services/chatService/__tests__/createMessage.test.ts
@@ -0,0 +1,32 @@
+import he from "he";
+import mongoose from "mongoose";
+import { Message } from "@models/Message";
+import { createMessage } from "../createMessage";
+
+describe("createMessage", () => {
+  beforeAll(async () => {
+    await mongoose.connect("mongodb://localhost:27017/chatdb");
+  });
+
+  afterAll(async () => {
+    await Message.deleteMany({});
+    await mongoose.connection.close();
+  });
+
+  it("should save a sanitized message to db", async () => {
+    const authorUsername = "user";
+    const content = "<script>${malicious}</tag>!!print()~^";
+    const chatId = "123";
+
+    const sanitizedContent = he.encode(content);
+    const foundMessage = await Message.findOne({
+      content: sanitizedContent,
+      chatId: chatId,
+      authorName: authorUsername,
+    });
+    const result = await createMessage(authorUsername, content, chatId);
+
+    expect(result.content).toEqual(sanitizedContent);
+    expect(foundMessage).toBeDefined;
+  });
+});
