d4d718876bed0b9024d259ea13ba126cafa5d4c3
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-03-15 20:46:23 +0100

    remove commented lines

--- backend/src/services/chatServices/fetchMessages.ts
+++ backend/src/services/chatServices/fetchMessages.ts
@@ -4,7 +4,6 @@ import { decodeContent } from "./helpers/decodeSanitizedMessage";
 export const fetchChatMessages = async (chatId: string) => {
   const messages = await getChatMessages(chatId);
 
-  //   const decodedMessages = messages;
   const decodedMessages = messages.map((message) => {
     return decodeContent(message.content);
   });
