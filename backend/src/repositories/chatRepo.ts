e2cc842e24fc3783fe3910a266b7a2a6af8a734c
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-03-15 17:30:51 +0100

    fix chat model import

--- backend/src/repositories/chatRepo.ts
+++ backend/src/repositories/chatRepo.ts
@@ -1,4 +1,4 @@
-import { Chat, IChat } from "models/Chat";
+import { Chat, IChat } from "../models/Chat";
 
 export const newChat = async (userIds: string[]): Promise<IChat> => {
   const chat = await Chat.create({ userIds });
