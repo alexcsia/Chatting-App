b646d3edc68feadaab0bf1f102a9bdf09c12c419
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-03-23 17:25:39 +0100

    retrieve full message document

--- backend/src/services/chatServices/fetchMessages.ts
+++ backend/src/services/chatServices/fetchMessages.ts
@@ -5,7 +5,8 @@ export const fetchChatMessages = async (chatId: string) => {
   const messages = await getChatMessages(chatId);
 
   const decodedMessages = messages.map((message) => {
-    return decodeContent(message.content);
+    const messageObject = message.toObject();
+    return { ...messageObject, content: decodeContent(message.content) };
   });
 
   return decodedMessages;
