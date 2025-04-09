0051017c166d46542f22edcbb23fbe014b55abea
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-04-09 20:15:04 +0200

    remove unecessary log

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export async function connectMongoDB() {
   try {
     const uri = process.env.MONGODB_URI;
-    console.log(uri + " connection string");
     if (uri) await mongoose.connect(uri, { dbName: "chatdb" });
     console.log("successfully connected to mongoDB");
   } catch (error: unknown) {
