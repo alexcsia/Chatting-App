0b2bd108161f38e70b41c376e76d4a4062ea70db
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-04-13 12:35:12 +0200

    remove profile route

--- backend/src/api/routes/user.routes.ts
+++ backend/src/api/routes/user.routes.ts
@@ -1,21 +1,10 @@
 import { FastifyInstance, FastifyPluginAsync } from "fastify";
-import { profileRequestSchema } from "./schemas/profile.schema";
-import { profileController } from "@api/controllers/profile";
 import { userInfoController } from "@api/controllers/userInfo";
 import { basicAuthSchema } from "./schemas/basicAuth.schema";
 
 export const userRoutes: FastifyPluginAsync = async (
   fastify: FastifyInstance
 ) => {
-  fastify.get(
-    "/profile/:username",
-    {
-      schema: profileRequestSchema,
-      onRequest: [fastify.verifyJWT],
-    },
-    profileController
-  );
-
   fastify.get(
     "/user-info",
     { schema: basicAuthSchema, onRequest: [fastify.verifyJWT] },
