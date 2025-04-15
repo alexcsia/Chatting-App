22ce7ebcc2e3687e6fc38cd356c97ca16ac34769
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-04-15 16:57:01 +0200

    add refresh token logic in interceptor

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 import { useUserStore } from "@/stores/user";
 import router from "@/router";
 
+const backendURL = import.meta.env.VITE_BACKEND_URL;
+
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: backendURL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -12,23 +14,36 @@ const apiClient = axios.create({
 
 apiClient.interceptors.response.use(
   (response) => response,
-  (error) => {
+  async (error) => {
     const userStore = useUserStore();
-    const requestUrl = error.config?.url;
-
+    const originalRequest = error.config;
+    const requestUrl = originalRequest?.url;
     const isAuthCheckRequest = requestUrl?.endsWith("/user-info");
 
-    if (error.response?.status === 401 && !isAuthCheckRequest) {
-      userStore.logout();
+    if (
+      error.response?.status === 401 &&
+      !isAuthCheckRequest &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
 
-      if (!router.currentRoute.value.path.startsWith("/login")) {
-        router.push({
-          path: "/login",
-          query: { redirect: router.currentRoute.value.fullPath },
+      try {
+        await axios.get(backendURL + "/api/auth/refresh-token", {
+          withCredentials: true,
         });
-      }
 
-      return new Promise(() => {});
+        return apiClient(originalRequest);
+      } catch (err) {
+        userStore.logout();
+        if (!router.currentRoute.value.path.startsWith("/login")) {
+          router.push({
+            path: "/login",
+            query: { redirect: router.currentRoute.value.fullPath },
+          });
+        }
+
+        return Promise.reject(err);
+      }
     }
 
     return Promise.reject(error);
