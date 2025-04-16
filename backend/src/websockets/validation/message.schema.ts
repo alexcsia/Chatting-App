dc865bbf6551a52a81f3b5ad96c9668f0cfc5a0a
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-04-16 20:32:38 +0200

    fix date validation

--- backend/src/websockets/validation/message.schema.ts
+++ backend/src/websockets/validation/message.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const messageSchema = z.object({
   authorUsername: z.string().min(1).max(20),
   content: z.string().min(1).max(1000),
-  timeStamp: z.date(),
+  timeStamp: z.preprocess((val) => new Date(val as number), z.date()),
   chatId: z.string().min(1),
 });
 
