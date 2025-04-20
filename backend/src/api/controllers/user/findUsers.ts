247711339a4915816ccf513f65ab3ffb5b6b9c60
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-04-21 00:01:07 +0200

    create finduser controller

---
+++ backend/src/api/controllers/user/findUsers.ts
@@ -0,0 +1,22 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { ApiError } from "@api/errors/ApiError";
+import { findUsersQuery } from "@api/routes/schemas/user/findUsers.schema";
+import userServices from "@services/userServices";
+
+export const findUsersController = async (
+  request: FastifyRequest<{ Params: findUsersQuery }>,
+  reply: FastifyReply
+) => {
+  try {
+    const usersToFind = request.params.username;
+
+    await userServices.findUsers(usersToFind);
+  } catch (error: unknown) {
+    if (error instanceof ApiError) {
+      reply.status(error.status).send({ message: error.message });
+    } else {
+      console.error("Unexpected error:", error);
+      return reply.status(500).send({ error: "An unexpected error occurred" });
+    }
+  }
+};
