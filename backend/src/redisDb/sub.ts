391ac28f06a0046050ed98bb279fcf46ee36af4e
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-04-26 15:23:22 +0200

    ensure only one redis subscription

--- backend/src/redisDb/sub.ts
+++ backend/src/redisDb/sub.ts
@@ -1,7 +1,12 @@
 import { sub } from "redisDb";
 import { IMessage } from "@models/Message";
 
+let subscribed = false;
+
 export const subscribeRedis = () => {
+  if (subscribed) return;
+  subscribed = true;
+
   sub.subscribe("chat", () => {
     console.log("subscribed to redis channel");
   });
