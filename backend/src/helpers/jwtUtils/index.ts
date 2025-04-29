370184327a6bc56f1a5a901ecccd33e032fdb911
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-04-29 22:31:36 +0200

    fix import issue

--- backend/src/helpers/jwtUtils/index.ts
+++ backend/src/helpers/jwtUtils/index.ts
@@ -1,5 +1,4 @@
-import { signJWT } from "./signJWT";
 import { signRefreshJWT } from "./signRefreshJWT";
 import { setAuthCookies } from "@api/controllers/auth/helpers/setAuthCookies";
 
-export default { signJWT, signRefreshJWT, setAuthCookies };
+export default { signRefreshJWT, setAuthCookies };
