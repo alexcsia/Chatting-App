4a40035b4eb2a553affdf2f8ee4f2527107fff01
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-04-29 03:20:40 +0200

    use refresh token secret

--- backend/src/helpers/jwtUtils/signRefreshJWT.ts
+++ backend/src/helpers/jwtUtils/signRefreshJWT.ts
@@ -3,7 +3,7 @@ import { ApiError } from "@api/errors/ApiError";
 
 export const signRefreshJWT = async (userId: string): Promise<string> => {
   try {
-    const REFRESH_TOKEN_SECRET = process.env.JWT_SECRET;
+    const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
     if (!REFRESH_TOKEN_SECRET) throw new Error("REFRESHJWT_SECRET not defined");
     const refreshToken = jwt.sign({ userId: userId }, REFRESH_TOKEN_SECRET, {
       expiresIn: "7d",
