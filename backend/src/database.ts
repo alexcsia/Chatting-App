fc449789350c13b54e2b626cc3357d8cfce6dae1
Alexia Bertalan <alexia.bertalan@code.berlin>
2025-04-29 03:25:33 +0200

    fix typo

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectMongoDB = async (mongoUri?: string) => {
-  const uri = mongoUri || process.env.MONGO_URI;
+  const uri = mongoUri || process.env.MONGODB_URI;
   if (!uri) {
     throw new Error("mongodb uri is missing");
   }
